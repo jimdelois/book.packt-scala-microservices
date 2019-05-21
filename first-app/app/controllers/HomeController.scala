c9fc2620b6af40c951f4a2af354c434e39d6508e
Jim DeLois <jimdelois@users.noreply.github.com>
2019-05-21 17:25:16 -0400

    A few more small examples

--- first-app/app/controllers/HomeController.scala
+++ first-app/app/controllers/HomeController.scala
@@ -10,17 +10,34 @@ class HomeController @Inject()(cc: ControllerComponents) extends AbstractControl
     Ok("Hello World!")
   }
 
-  def hello(name: String) = Action{
+  // As per Scala, Action {...} is syntactic sugar for "Action.apply{...}"
+  def hello(name: String) = Action {
     Ok(s"Hello, $name!")
   }
 
+
   def addUser() = Action { implicit request =>
     val body = request.body
+
+    // Could be:
+    // application/x-www-form-urlencoded => request.body.asFormUrlEncoded
+    // text/plain => request.body.asText
+    // application/xml => request.body.asXML
+    // application/json => request.body.asJson
 
     body.asFormUrlEncoded match{
       //persist user information
       case Some(map) => Ok(s"The user of name `${map("name").head}` and age `${map("age").head}` has been created\n")
       case None => BadRequest("Unknow body format")
+    }
+  }
+
+  // Some examples of other responses...
+  def sqrt(num:String) = Action {
+    scala.util.Try(num.toInt) match {
+      case scala.util.Success(ans) if ans >= 0 => Ok(s"The answer is: ${math.sqrt(ans)}")
+      case scala.util.Success(ans) => BadRequest(s"The input ($num) must be greater than zero")
+      case scala.util.Failure(ex) => InternalServerError(s"Could not extract the contents from $num")
     }
   }
 }
