aac3b2cbee3a3d0dad75da109bf012c6efe31f2c
Jim DeLois <jimdelois@users.noreply.github.com>
2019-05-21 16:06:32 -0400

    Adding POST Example

--- first-app/app/controllers/HomeController.scala
+++ first-app/app/controllers/HomeController.scala
@@ -14,5 +14,13 @@ class HomeController @Inject()(cc: ControllerComponents) extends AbstractControl
     Ok(s"Hello, $name!")
   }
 
+  def addUser() = Action { implicit request =>
+    val body = request.body
 
+    body.asFormUrlEncoded match{
+      //persist user information
+      case Some(map) => Ok(s"The user of name `${map("name").head}` and age `${map("age").head}` has been created\n")
+      case None => BadRequest("Unknow body format")
+    }
+  }
 }
